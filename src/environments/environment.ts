7f37940f103729b5c94d089bc9b04badf1301929
carlos <vilcacutipacarlosantonio@gmail.com>
2024-12-19 16:31:04 -0500

    update environmnet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  wsUrl: "https://pedi2.pe:5000",
-  dataUrl: "https://pedi2.pe:5000",
+  wsUrl: "https://panel.pedi2.pe",
+  dataUrl: "https://panel.pedi2.pe",
   facturaUrl: "http://app.factura.vip/api/client",
   versionApp:"1.0.16",
   tokenMapa: "sk.eyJ1IjoiamhvbndqIiwiYSI6ImNsaDBycHN6NzB3NWQzZnA3bm93bnJqdTIifQ.lu3qfaK4lH69UW9uLuGx4g"
